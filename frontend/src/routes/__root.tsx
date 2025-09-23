ae44d8fb507d155a59dc92be9d2f10895bbeca08
Gabriel Nakaema <gsnakaema@gmail.com>
2025-09-22 22:02:24 -0300

    fix(frontend): check environment

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,27 +1,48 @@
-import { TanStackDevtools } from '@tanstack/react-devtools';
-import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools';
-import { Outlet, createRootRoute } from '@tanstack/react-router';
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { lazy, Suspense } from 'react';
+
+const isDev = import.meta.env.DEV;
+
+const devComponents = isDev
+  ? {
+      TanStackDevtools: lazy(() =>
+        import('@tanstack/react-devtools').then((module) => ({ default: module.TanStackDevtools })),
+      ),
+      TanStackRouterDevtoolsPanel: lazy(() =>
+        import('@tanstack/react-router-devtools').then((module) => ({ default: module.TanStackRouterDevtoolsPanel })),
+      ),
+      ReactQueryDevtoolsPanel: lazy(() =>
+        import('@tanstack/react-query-devtools').then((module) => ({ default: module.ReactQueryDevtoolsPanel })),
+      ),
+    }
+  : { TanStackDevtools: null, TanStackRouterDevtoolsPanel: null, ReactQueryDevtoolsPanel: null };
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-left',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          {
-            name: 'Tanstack Query',
-            render: <ReactQueryDevtoolsPanel />,
-          },
-        ]}
-      />
+      {isDev && devComponents.TanStackDevtools && (
+        <Suspense fallback={null}>
+          (
+          <devComponents.TanStackDevtools
+            config={{
+              position: 'bottom-left',
+              defaultOpen: false,
+            }}
+            plugins={[
+              {
+                name: 'Tanstack Router',
+                render: <devComponents.TanStackRouterDevtoolsPanel />,
+              },
+              {
+                name: 'Tanstack Query',
+                render: <devComponents.ReactQueryDevtoolsPanel />,
+              },
+            ]}
+          />
+          )
+        </Suspense>
+      )}
     </>
   ),
 });
